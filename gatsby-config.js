ff611cc2346ea7fa6b7560b1dc1594d72c877dfe
Jayrah DeVore <legowave440@users.noreply.github.com>
2022-09-10 16:53:41 -0400

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     title: `gatsby_tutorial`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
